1d5f619b74ffd5204bf107cb6fd42378def58380
Ananda Ryanizar <anandar.ryanizar@gmail.com>
2020-10-12 09:39:55 +0700

    change remove prefix logic

--- face/compare.py
+++ face/compare.py
@@ -1,5 +1,11 @@
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
+
 def compare(source, target, client):
-    source = str(source).lstrip("https://gobase-groupa.s3.us-east-2.amazonaws.com/")
+    source = remove_prefix(source, "https://gobase-groupa.s3.us-east-2.amazonaws.com/")
     response = client.compare_faces(SimilarityThreshold=90,
                                     SourceImage={"S3Object": {
                                         "Bucket": "gobase-groupa",
@@ -7,5 +13,13 @@ def compare(source, target, client):
                                     }},
                                     TargetImage={'Bytes': target})
     if len(response['FaceMatches']) > 0:
+
         return True
     return False
+
+# aws rekognition detect-labels \                                                                                                        ✔  305  09:21:57
+# --image '{"S3Object":{"Bucket":"gobase-groupa","Name":"media/2493f050-0c2a-11eb-b99b-0a05adc1166c.jpg"}}'
+
+# '{"SimilarityThreshold": 90,"SourceImage": {"S3Object": {"Bucket":"gobase-groupa","Name": "media/2493f050-0c2a-11eb-b99b-0a05adc1166c.jpg"}},"TargetImage": { "Bytes": blob,"S3Object": {"Bucket": "gobase-groupa","Name": "media/2493f050-0c2a-11eb-b99b-0a05adc1166c.jpg"}}}'
+
+# --image '{"S3Object":{"Bucket":"gobase-groupa","Name":"media/2493f050-0c2a-11eb-b99b-0a05adc1166c.jpg"}}'
